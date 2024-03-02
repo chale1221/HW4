a6d79516780edbd021fc5ed931a83b67eba13c86
Carter Hale <carter.hale@kellogg.northwestern.edu>
2024-03-02 17:05:54 +0000

    HW 4 - Commit 1

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,17 +6,19 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    @entries = Entry.where({ "place_id" => @place["id"], "user_id" => @current_user["id"] })
   end
 
   def new
   end
 
   def create
+    @user = User.find_by({"id" => session["user_id"]})
+    if @user != nil
     @place = Place.new
     @place["name"] = params["name"]
     @place.save
     redirect_to "/places"
+    end
   end
-
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,30 @@
 class SessionsController < ApplicationController
+  def create
+    # authenticate user
+    @user = User.find_by({ "email" => params["email"]})
+    if @user != nil
+        if BCrypt::Password.new(@user["password"]) == params["password"]
+          session["user_id"] = @user["id"]
+          flash["notice"] = "Welcome, #{@user["username"]}."
+          redirect_to "/places"
+        else
+          flash["notice"] = "Wrong username or password."
+          redirect_to "/login"
+        end
+    end
+  end
+
   def new
   end
 
-  def create
+    #logout user
+  def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye."
+    redirect_to "/login"
   end
 
-  def destroy
-  end
+ 
 end
+
   
